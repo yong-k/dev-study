f6c1179a12ddd9934d32e9e44bc0c9271b291c82
jykim <jykim@iotree.co.kr>
2024-08-08 15:16:06 +0900

    환경세팅

---
+++ vue-tabulator/src/main/java/com/dev/vue_tabulator/VueTabulatorApplication.java
@@ -0,0 +1,13 @@
+package com.dev.vue_tabulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueTabulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueTabulatorApplication.class, args);
+	}
+
+}
