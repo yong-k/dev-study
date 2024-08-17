0956710c36bc698c61658de4d5f94f71e642da1a
jeongyong <cin03023@gmail.com>
2024-08-18 03:00:28 +0900

    DDL, Vo 수정

--- vue-tabulator/DDL.sql
+++ vue-tabulator/DDL.sql
@@ -1,11 +1,20 @@
 CREATE DATABASE `tabulator`;
 CREATE TABLE `user` (
-  `id` varchar(100) NOT NULL,
-  `name` varchar(100) DEFAULT NULL,
-  `age` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+    `id` varchar(100) NOT NULL,
+    `name` varchar(100) DEFAULT NULL,
+    `Date` date DEFAULT NULL,
+    `Quantity` int(11) DEFAULT NULL,
+    `Price` int(11) DEFAULT NULL,
+    PRIMARY KEY (`id`)
 );
 
-INSERT INTO `user`(id, name, age) VALUES('id100', 'Bob Oil', 32);
-INSERT INTO `user`(id, name, age) VALUES('id101', 'May Mary', 11);
-INSERT INTO `user`(id, name, age) VALUES('id102', 'Philps B', 43);
+INSERT INTO tabulator.`user` (id, name, `Date`, Quantity, Price)
+VALUES ('#Cust1', '홍길동', '2014-10-01', 50, 3630700);
+INSERT INTO tabulator.`user` (id, name, `Date`, Quantity, Price)
+VALUES ('#Cust2', 'Steve', '2024-01-01', 20, 630800);
+INSERT INTO tabulator.`user` (id, name, `Date`, Quantity, Price)
+VALUES ('#Cust3', 'Bill', '2023-05-07', 100, 630000);
+INSERT INTO tabulator.`user` (id, name, `Date`, Quantity, Price)
+VALUES ('#Cust4', '홍길동', '2014-11-01', 70, 150000);
+INSERT INTO tabulator.`user` (id, name, `Date`, Quantity, Price)
+VALUES ('#Cust5', 'Steve', '2020-10-10', 55, 1000000);
--- vue-tabulator/src/main/java/com/dev/vue_tabulator/vo/UserVo.java
+++ vue-tabulator/src/main/java/com/dev/vue_tabulator/vo/UserVo.java
@@ -4,11 +4,15 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 @Data
 @NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserVo {
     private String id;
     private String name;
-    private Integer age;
+    private Date date;
+    private Integer quantity;
+    private Integer price;
 }
