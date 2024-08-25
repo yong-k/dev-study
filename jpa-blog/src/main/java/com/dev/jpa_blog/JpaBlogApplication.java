295c238966f5c591f3dd6f8d29eb987b21c3af98
jeongyong <cin03023@gmail.com>
2024-08-26 00:26:35 +0900

    intial commit

---
+++ jpa-blog/src/main/java/com/dev/jpa_blog/JpaBlogApplication.java
@@ -0,0 +1,13 @@
+package com.dev.jpa_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaBlogApplication.class, args);
+	}
+
+}
---
+++ jpa-blog/src/test/java/com/dev/jpa_blog/JpaBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.jpa_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
